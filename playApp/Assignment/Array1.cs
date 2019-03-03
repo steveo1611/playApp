5d69ad97be455af1738f024faf805b1a8169fde4
Steven Oldfield <steveold1611@gmail.com>
2019-03-02 20:00:36 -0700

    Add project files.

---
+++ playApp/Assignment/Array1.cs
@@ -0,0 +1,14 @@
+﻿//  Assignment 2
+// add elements in array - create an array of interger and add few user inputs.
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace playApp.Assignment
+{
+    public class Array1
+    {
+
+
+    }
+}
---
+++ playApp/Program.cs
@@ -0,0 +1,33 @@
+﻿// assignment 1
+//  create a string value of 'n' that is the output the sum of 'n'
+//  Assignment 2
+// add elements in array - create an array of interger and add few user inputs.
+using System;
+
+namespace playApp
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string inputString = "";
+            string outputString = "";
+
+            Console.WriteLine("Hello, Please enter something: or type 'exit' to end program");
+            inputString = Console.ReadLine();
+            while (inputString.ToLower() != "exit")
+            {
+                if (inputString == "")
+                {
+                    Console.WriteLine("Uh nothing is NOT something");
+                }
+                outputString += inputString;
+                Console.WriteLine(outputString);
+                Console.WriteLine("Please enter something else or type 'exit' to end program");
+                inputString = Console.ReadLine();
+            }
+            Console.WriteLine("Goodbye!!!");
+            Console.ReadLine();
+        }
+    }
+}
