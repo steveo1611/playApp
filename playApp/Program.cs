d9ba9736b3f95ef82bfb165bae97fe1782af2577
Steven Oldfield <steveold1611@gmail.com>
2019-03-05 18:01:52 -0700

    making some changes

--- playApp/Program.cs
+++ playApp/Program.cs
@@ -10,23 +10,34 @@ namespace playApp
     {
         static void Main(string[] args)
         {
-            string inputString = "";
-            string outputString = "";
+            /*  Assignment #1 - the value of 'n'
+                        string inputString = "";
+                        string outputString = "";
 
-            Console.WriteLine("Hello, Please enter something: or type 'exit' to end program");
-            inputString = Console.ReadLine();
-            while (inputString.ToLower() != "exit")
+                        Console.WriteLine("Hello, Please enter something: or type 'exit' to end program");
+                        inputString = Console.ReadLine();
+                        while (inputString.ToLower() != "exit")
+                        {
+                            if (inputString == "")
+                            {
+                                Console.WriteLine("Uh nothing is NOT something");
+                            }
+                            outputString += inputString;
+                            Console.WriteLine(outputString);
+                            Console.WriteLine("Please enter something else or type 'exit' to end program");
+                            inputString = Console.ReadLine();
+                        }
+                        Console.WriteLine("Goodbye!!!");
+                        Console.ReadLine();
+             */
+            /*  Assignment #2 - Array of integer  */
+            int[] arr = new int[1];
+            
+            // see examples in tutorialspoint for apps and loops
+            for (int i = 0; i < arr.length; i++)
             {
-                if (inputString == "")
-                {
-                    Console.WriteLine("Uh nothing is NOT something");
-                }
-                outputString += inputString;
-                Console.WriteLine(outputString);
-                Console.WriteLine("Please enter something else or type 'exit' to end program");
-                inputString = Console.ReadLine();
+                Console.WriteLine("contents: {0}", i);
             }
-            Console.WriteLine("Goodbye!!!");
             Console.ReadLine();
         }
     }
